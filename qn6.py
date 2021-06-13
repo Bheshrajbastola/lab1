2b1b0e4c39d1ba2cf9f313f3eb7bc6cdae23606a
dell <BOB-ctrl-creator>
2021-06-13 13:01:52 +0545

    qnno6

---
+++ qn6.py
@@ -0,0 +1,8 @@
+''' 6. Solve each of the following problems using Python Scripts.
+Make sure you use appropriate variable names and comments.
+When there is a final answer have Python print it to the screen.
+A person’s body mass index (BMI) is defined as:
+BMI=mass in kg / (height in m)2.'''
+mass=float(input("enter a mass in kg"))
+height=float(input("enter a height"))
+print("your body mass",mass/(height*height))
