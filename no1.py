af0eb72755dcbfb4e93c12491293f03bee4e64d2
dell <BOB-ctrl-creator>
2021-06-13 12:58:05 +0545

    qnno1

---
+++ no1.py
@@ -0,0 +1,8 @@
+#write a program that takes three number and print their sum. Every number is given in seprete line.
+
+num1 =int (input("enter first number"))
+num2 =int (input ("enter second number"))
+num3 =int (input ("enter third number"))
+sum = num1+num2+num3
+print(sum)
+print((f"the sum{0}"))
