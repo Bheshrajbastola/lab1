f14fdf411a709389babbe02274f4d891b5c1f9fc
dell <BOB-ctrl-creator>
2021-06-13 12:59:17 +0545

    qnno2

---
+++ no2.py
@@ -0,0 +1,7 @@
+#Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+# Every number is given on a separate line.
+num1 =int(input("enter the base"))
+num2 =int(input("enetr the height"))
+area=num1*num2/2
+print(area)
+
